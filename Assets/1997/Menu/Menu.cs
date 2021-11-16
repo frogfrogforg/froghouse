4398cc255b5842b75047c6a7f9ba822de73b3eed
Ty Cobb <ty.cobb.m@gmail.com>
2021-11-15 19:14:04 -0500

    frogress

---
+++ Assets/1997/Menu/Menu.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Frog1997 {
+
+/// the menu
+public class Menu : MonoBehaviour {
+    // -- nodes --
+    [Header("nodes")]
+    [Tooltip("the limit progress bar")]
+    [SerializeField] MenuProgress m_Limit;
+
+    [Tooltip("the time progress bar")]
+    [SerializeField] MenuProgress m_Time;
+
+    // -- lifecycle --
+    void Update() {
+        m_Limit.Pct = Time.time / 30.0f;
+        m_Time.Pct = Mathf.Repeat(Time.time, 5.0f) / 5.0f;
+    }
+}
+
+}
