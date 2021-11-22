dbdc4cc96b2574ffbfe2dc5dccc21f60f38e52ad
fran <franmorojo@gmail.com>
2021-11-22 11:02:01 -0500

    holding W makes the frog move backwards

--- Assets/2001/FrogController.cs
+++ Assets/2001/FrogController.cs
@@ -7,10 +7,11 @@ namespace Frog2001
 {
     public class FrogController : MonoBehaviour
     {
+        public float moveSpeed;
 
         private Animator anim;
 
-        
+
         // Start is called before the first frame update
         void Start()
         {
@@ -21,9 +22,18 @@ namespace Frog2001
         void Update()
         {
 
+            //var position = transform.position;
+
            if (Input.GetKey(KeyCode.W)) {
                Debug.Log("move frog forward");
+               transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
            }   
+
+        
+
+        
+
+
         }
     }
 }
