8152a5fe67feffd80ea453a7fa82edb09610bcc5
fran <franmorojo@gmail.com>
2021-11-22 10:51:41 -0500

    add cinemachine brain

--- Assets/2001/FrogController.cs
+++ Assets/2001/FrogController.cs
@@ -7,10 +7,14 @@ namespace Frog2001
 {
     public class FrogController : MonoBehaviour
     {
+
+        private Animator anim;
+
+        
         // Start is called before the first frame update
         void Start()
         {
-            
+            anim = GetComponent<Animator>();
         }
 
         // Update is called once per frameif
@@ -19,7 +23,6 @@ namespace Frog2001
 
            if (Input.GetKey(KeyCode.W)) {
                Debug.Log("move frog forward");
-
            }   
         }
     }
